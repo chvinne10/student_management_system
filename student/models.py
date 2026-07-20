0800140f26539ab38f61dcce9e424008516d1a4e
chvinne10 <chvinaykumarchviti@gmail.com>
2026-07-20 09:00:11 +0530

    chanage  database

--- student/models.py
+++ student/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     name=models.CharField( max_length=50)
-    rollno=models.CharField(max_length=40)
+    rollno=models.CharField(max_length=40, unique=True)
     student_branch=models.CharField( max_length=50)
     section=models.CharField(max_length=20)
     place=models.CharField(max_length=60)
